14f2bbb80a81fee60bcfa0a5bf03217a43f962d9
navin-nawshin <navinmdnawshin@gmail.com>
2024-11-13 23:38:20 +0600

    new wrapper

--- script.js
+++ script.js
@@ -2,17 +2,14 @@ function toggleMenu(){
     document.getElementById("menu").classList.toggle("show");
 }
 document.querySelectorAll('.project_links a').forEach(link => {
-    link.addEventListener('mouseenter', function() {
+    link.addEventListener('mouseenter', function(){
         const img = link.querySelector('img');
-        // Store the original src in a data attribute
         img.dataset.originalSrc = img.src;
-        // Change the src to the new image
-        img.src = "https://img.icons8.com/?size=100&id=62856&format=png&color=508C9B";
+        img.src = "https://img.icons8.com/?size=100&id=62856&format=png&color=A64D79";
     });
 
-    link.addEventListener('mouseleave', function() {
+    link.addEventListener('mouseleave', function(){
         const img = link.querySelector('img');
-        // Restore the original src from the data attribute
         img.src = img.dataset.originalSrc;
     });
 });
