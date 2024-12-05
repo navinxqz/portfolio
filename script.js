992eabf6c3fd45b399891a47cd2c963797d34555
navin-nawshin <navinmdnawshin@gmail.com>
2024-12-06 00:01:39 +0600

    contact section added

--- script.js
+++ script.js
@@ -13,24 +13,4 @@ document.querySelectorAll('.project_links a').forEach(link => {
         img.src = img.dataset.originalSrc;
     });
 });
-
-function sendEmail() {
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-  
-    Email.send({
-      SecureToken: "your-secure-token",
-      To: "cyt.navin6@gmail.com",
-      From: email,
-      Subject: "New Contact Form Submission",
-      Body: `Name: ${name} <br> Email: ${email} <br> Message: ${message}`,
-    }).then((response) => {
-      if (response === "OK") {
-        alert("Your message has been sent successfully!");
-      } else {
-        alert("There was an error sending your message. Please try again.");
-      }
-    });
-  }
   
